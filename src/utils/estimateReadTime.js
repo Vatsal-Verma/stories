7bdece1bd568a4b8c788397bf84fa8a89e333a9a
Vatsal Verma <146416700+Vatsal-Verma@users.noreply.github.com>
2026-02-23 23:10:07 +0530

    triggering ci build
    Updated regex to remove markdown links more broadly.

--- src/utils/estimateReadTime.js
+++ src/utils/estimateReadTime.js
@@ -7,7 +7,7 @@ export default function estimateReadTime(paragraphs = [], wordsPerMinute = 200) 
         .join(' ');
     const cleanedText = fullText
         .replace(/<[^>]*>/g, ' ') // remove HTML tags
-        .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '$1') // remove markdown links
+        .replace(/\[([^\]]+)\]\(([^)]+)\)/g, '$1') // remove markdowns
         .replace(/https?:\/\/\S+/g, '') // remove raw URLs
         .replace(/[`*_>#-]/g, '') // remove markdown symbols
         .replace(/\s+/g, ' ')
